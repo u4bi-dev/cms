12a88626d793e564038115ed0e0d4d5fe98e3be9
u4bi <rootcode10@gmail.com>
2017-11-07 19:26:21 +0900

    scroll view render

--- src/app/pages/board/board-list/board-list.component.ts
+++ src/app/pages/board/board-list/board-list.component.ts
@@ -9,8 +9,20 @@ import { RouterService } from '../../../providers/router.service';
 })
 export class BoardListComponent implements OnInit {
   
-  rows : any[] = [];
+  rows : any[] = [
+    { "boardId" : 'programming', "id"    : 1, "title" : "1번글 제목 테스트입니다.", "write" : "ㅇㅇ", "date"  : "18:23", "hits"  : 1023, "likes"  : 10 },
+    { "boardId" : 'programming', "id"    : 2, "title" : "2번글 제목 테스트입니다.", "write" : "ㅇㅇ", "date"  : "18:23", "hits"  : 1023, "likes"  : 10 },
+    { "boardId" : 'programming', "id"    : 3, "title" : "3번글 제목 테스트입니다.", "write" : "ㅇㅇ", "date"  : "18:23", "hits"  : 1023, "likes"  : 10 },
+    { "boardId" : 'programming', "id"    : 4, "title" : "4번글 제목 테스트입니다.", "write" : "ㅇㅇ", "date"  : "18:23", "hits"  : 1023, "likes"  : 10 },
+    { "boardId" : 'programming', "id"    : 5, "title" : "5번글 제목 테스트입니다.", "write" : "ㅇㅇ", "date"  : "18:23", "hits"  : 1023, "likes"  : 10 },
+    { "boardId" : 'programming', "id"    : 6, "title" : "6번글 제목 테스트입니다.", "write" : "ㅇㅇ", "date"  : "18:23", "hits"  : 1023, "likes"  : 10 },
+    { "boardId" : 'programming', "id"    : 7, "title" : "7번글 제목 테스트입니다.", "write" : "ㅇㅇ", "date"  : "18:23", "hits"  : 1023, "likes"  : 10 },
+    { "boardId" : 'programming', "id"    : 8, "title" : "8번글 제목 테스트입니다.", "write" : "ㅇㅇ", "date"  : "18:23", "hits"  : 1023, "likes"  : 10 },
+    { "boardId" : 'programming', "id"    : 9, "title" : "9번글 제목 테스트입니다.", "write" : "ㅇㅇ", "date"  : "18:23", "hits"  : 1023, "likes"  : 10 }
+  ];
   boardName : string;
+
+  offset : number = 0;
   
   constructor(private route: ActivatedRoute, public routerService : RouterService) {
 
@@ -18,24 +30,32 @@ export class BoardListComponent implements OnInit {
 
   ngOnInit(){
       this.route.parent.params.subscribe( (param: any) => this.boardName = param['id'] );
-      
-      let data = {
-        "boardId" : this.boardName,
-        "id"    : 1,
-        "title" : "제목 테스트입니다.",
-        "write" : "ㅇㅇ",
-        "date"  : "18:23",
-        "hits"  : 1023,
-        "likes"  : 10
-      };
-
-      for(let i = 20; i--;)this.rows.push(data);
 
   }
 
   onPage(e) {
-      console.log('page', e);
-      document.documentElement.scrollTop = 0;      
+    if(this.offset <= e.offset && this.offset !== e.offset){
+      this.offset = e.offset;
+
+      setTimeout(()=>{
+          for(let i = 9; i--;){
+              this.rows.push({
+                "boardId" : this.boardName,
+                "id"    : this.rows.length+1,
+                "title" : `${ this.rows.length+1 }번글 제목 테스트입니다.`,
+                "write" : "ㅇㅇ",
+                "date"  : "18:23",
+                "hits"  : 1023,
+                "likes"  : 10
+              });
+          }
+
+      }, 300);
+      
+    }
+    
+    // document.documentElement.scrollTop = 0;      
+
   }
 
   onActivate(e) {
