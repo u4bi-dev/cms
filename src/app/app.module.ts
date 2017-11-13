4218ad4ac7a50475f1f54a1fdb59a6d001f81972
u4bi <rootcode10@gmail.com>
2017-11-13 18:30:55 +0900

    update package module
    - ngrx-store
    - ngrx-effects
    - ngrx-store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { MatTooltipModule, MatToolbarModule, MatIconModule } from '@angular/material';
 
 import { RouterService } from './providers/router.service';
+import { boardPanelReducer } from './providers/boardPanel/boardPanel.reducer';
+import { BoardPanelEffects } from './providers/boardPanel/boardPanel.effects';
+import { BoardPanelService } from './providers/boardPanel/boardPanel.service';
+import { BoardPanelMockUPService } from './providers/boardPanel/model/boardPanel-mockup-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,12 +29,21 @@ import { FooterComponent } from './shared/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot({
+      boardPanel : boardPanelReducer
+    }),
+    EffectsModule.forRoot([BoardPanelEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10
+    }),
     MatTooltipModule,
     MatToolbarModule,
     MatIconModule
   ],
   providers: [
-    RouterService
+    RouterService,
+    BoardPanelService,
+    BoardPanelMockUPService
   ],
   bootstrap: [AppComponent]
 })
