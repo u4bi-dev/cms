f49795cee89b9fe54d45a0be40b3e614ba6dbc7f
u4bi <rootcode10@gmail.com>
2017-11-18 23:19:23 +0900

    write component redux data bind

--- src/app/pages/board/board-write/board-write.component.ts
+++ src/app/pages/board/board-write/board-write.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { GetDetailBoardPanel } from '../../../providers/boardPanel/boardPanel.actions';
+import { BoardPanelModel } from '../../../providers/boardPanel/boardPanel.model';
+
+interface MainState{
+  boardPanel : any;
+}
 
 @Component({
   selector: 'app-board-write',
   templateUrl: './board-write.component.html',
   styleUrls: ['./board-write.component.css']
 })
-export class BoardWriteComponent implements OnInit {
+export class BoardWriteComponent implements OnInit, OnDestroy {
   
-  boardName : string;
   private editor;
   public editorOptions = {
     placeholder: "작성해주세요..",
@@ -26,10 +34,30 @@ export class BoardWriteComponent implements OnInit {
     }
   };
 
-  constructor(private route: ActivatedRoute) { }
+  private boardPanel$ : Observable<any>;
+  private sub : Subscription;
+
+  detailBoard : BoardPanelModel;
+
+  constructor(private route: ActivatedRoute, private store : Store<MainState>) {
+
+    this.boardPanel$ = this.store.select('boardPanel');
+    this.sub = this.boardPanel$.subscribe(e => {
+        if(e){
+            this.detailBoard = e.detailBoard;
+        }
+    });
+
+  }
+
+  ngOnDestroy(){
+    this.sub.unsubscribe();
+  }
 
   ngOnInit() {
-    this.route.parent.params.subscribe( (param: any) => this.boardName = param['id'] );
+    this.route.parent.params.subscribe( (param: any) => {
+      this.store.dispatch(new GetDetailBoardPanel(param['id']));
+    });
     
   }
 
