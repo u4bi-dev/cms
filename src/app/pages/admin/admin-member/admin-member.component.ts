1698821c2028b215179e2595d37392f146c087b5
u4bi <rootcode10@gmail.com>
2017-11-22 19:02:19 +0900

    admin member component prototype render

--- src/app/pages/admin/admin-member/admin-member.component.ts
+++ src/app/pages/admin/admin-member/admin-member.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-admin-member',
-  templateUrl: './admin-member.component.html',
-  styleUrls: ['./admin-member.component.css']
+selector: 'app-admin-member',
+templateUrl: './admin-member.component.html',
+styleUrls: ['./admin-member.component.css']
 })
 export class AdminMemberComponent implements OnInit {
+    
+    rows = [];
+    temp = [];
 
-  constructor() { }
+    columns = [
+        { prop: 'name' },
+        { name: 'Rate' },
+        { name: 'Datetime' }
+    ];
+    
+    @ViewChild('table') table: any;
 
-  ngOnInit() {
-  }
+    constructor() {
+        for(let i = 40; i--;)this.rows.push({
+            name    : '홍길동' + i,
+            rate    : 200 + i,
+            datetime : '11/13'
+        });
+
+        this.temp = [...this.rows];
+        
+    }
+    
+    ngOnInit() {
+    }
+
+    updateFilter(event) {
+        const val = event.target.value.toLowerCase();
+        const temp = this.temp.filter( (d) => {
+
+            let check = (data) => data.indexOf(val) !== -1 || !val;
+            return check(d.name.toLowerCase()) || check(d.rate.toString().toLowerCase()) || check(d.datetime.toLowerCase());
+            
+        });
+
+        this.rows = temp;
+        this.table.offset = 0;
+    }
 
 }
